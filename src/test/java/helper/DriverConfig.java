23d2b7854de55ccc0ef58d53149cc97abd48e87f
faizul-islam <faizulcse@gmail.com>
2019-07-28 02:02:49 +0600

    adding scrolling functions

--- src/test/java/helper/DriverConfig.java
+++ src/test/java/helper/DriverConfig.java
@@ -1,7 +1,9 @@
 package helper;
 
+//import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
@@ -65,6 +67,8 @@ public class DriverConfig {
         {
             driver.get(URL);
             js = (JavascriptExecutor) driver;
+//            Dimension d = new Dimension(521,628);
+//            driver.manage().window().setSize(d);
             driver.manage().window().maximize();
             driver.manage().deleteAllCookies();
         }
@@ -73,5 +77,93 @@ public class DriverConfig {
     @AfterMethod
     public void tearDown() {
         if(driver!=null) driver.quit();
+    }
+
+    public static void scrollUp(){
+        int i = 1;
+        while (i <= 100) {
+            js.executeScript("window.scrollBy(0, -1)", "");
+            waitFor(1.0);
+            i++;
+        }
+    }
+
+    public static void scrollUp(int count){
+        int i = 0;
+        while (i < count * 100) {
+            js.executeScript("window.scrollBy(0, -1)", "");
+            waitFor(1.0);
+            i++;
+        }
+    }
+
+    public static void scrollDown(){
+        int i = 0;
+        while (i < 100) {
+            js.executeScript("window.scrollBy(0, 1)", "");
+            waitFor(1.0);
+            i++;
+        }
+    }
+
+    public static void scrollDown(int count){
+        int i = 0;
+        while (i < 100 * count) {
+            js.executeScript("window.scrollBy(0, 1)", "");
+            waitFor(1.0);
+            i++;
+        }
+    }
+
+    public static void scrollLeft(){
+        int i = 0;
+        while (i < 100) {
+            js.executeScript("window.scrollBy(-1, 0)", "");
+            waitFor(1.0);
+            i++;
+        }
+    }
+
+    public static void scrollLeft(int count){
+        int i = 0;
+        while (i < 100 * count) {
+            js.executeScript("window.scrollBy(-1, 0)", "");
+            waitFor(1.0);
+            i++;
+        }
+    }
+
+    public static void scrollRight(){
+        int i = 0;
+        while (i < 100) {
+            js.executeScript("window.scrollBy(1, 0)", "");
+            waitFor(1.0);
+            i++;
+        }
+    }
+
+    public static void scrollRight(int count){
+        int i = 0;
+        while (i < 100 * count) {
+            js.executeScript("window.scrollBy(1, 0)", "");
+            waitFor(1.0);
+            i++;
+        }
+    }
+
+    public static void waitFor(double milliSecond){
+        try {
+            Thread.sleep((long) milliSecond);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void waitFor(int second){
+        try {
+            Thread.sleep((int) second*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
